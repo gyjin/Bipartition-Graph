13a9e902fa9c1b4531cbb1fada96890c89b446e5
Ga-Young Jin <gyjinn@gmail.com>
2020-04-25 23:20:09 -0700

    completed bipartition

--- lib/possible_bipartition.rb
+++ lib/possible_bipartition.rb
@@ -1,4 +1,28 @@
-
 def possible_bipartition(dislikes)
-  raise NotImplementedError, "possible_bipartition isn't implemented yet"
+  groups = []
+  
+  counter = 0
+  until counter > dislikes.length - 1
+    if groups[counter].nil?
+      groups[counter] = true
+    end
+    
+    if dislikes[counter]
+      return false if !check_unfriendlies(dislikes, counter, groups)
+    end
+    counter += 1
+  end
+  
+  return true
+end
+
+def check_unfriendlies(dislikes, counter, groups)
+  dislikes[counter].each do |unfriendly|
+    if groups[unfriendly].nil?
+      groups[unfriendly] = !groups[counter]
+      return false if !check_unfriendlies(dislikes, unfriendly, groups)
+    elsif groups[unfriendly] == groups[counter]
+      return false
+    end
+  end
 end
--- test/possible_bipartition_test.rb
+++ test/possible_bipartition_test.rb
@@ -4,89 +4,89 @@ describe "possible_bipartition" do
   it "will work for example 1" do
     # Arrange
     dislikes = [ [],
-      [2, 3],
-      [1, 4],
-      [1],
-      [2]
-    ]
+    [2, 3],
+    [1, 4],
+    [1],
+    [2]
+  ]
+  
+  # Act
+  answer = possible_bipartition(dislikes)
+  
+  # Assert
+  expect(answer).must_equal true
+end
 
-    # Act
-    answer = possible_bipartition(dislikes)
+it "will work for example 2" do
+  # Arrange
+  dislikes =  [ [],
+  [2, 3],
+  [1, 3],
+  [1, 2]
+]
 
-    # Assert
-    expect(answer).must_equal true
-  end
+# Act
+answer = possible_bipartition(dislikes)
 
-  it "will work for example 2" do
-    # Arrange
-    dislikes =  [ [],
-      [2, 3],
-      [1, 3],
-      [1, 2]
-    ]
+# Assert
+expect(answer).must_equal false
+end
 
-    # Act
-    answer = possible_bipartition(dislikes)
+it "will work for example 3" do
+  # Arrange
+  dislikes = [ [],
+  [2, 5],
+  [1, 3],
+  [2, 4],
+  [3, 5],
+  [1, 4]
+]
 
-    # Assert
-    expect(answer).must_equal false
-  end
+# Act
+answer = possible_bipartition(dislikes)
 
-  it "will work for example 3" do
-    # Arrange
-    dislikes = [ [],
-      [2, 5],
-      [1, 3],
-      [2, 4],
-      [3, 5],
-      [1, 4]
-    ]
+# Assert
+expect(answer).must_equal false
+end
 
-    # Act
-    answer = possible_bipartition(dislikes)
+it "will return true for a graph which can be bipartitioned" do
+  # Arrange
+  dislikes = [ [3, 6],
+  [2, 5],
+  [1, 3],
+  [0, 2],
+  [5],
+  [1, 4],
+  [0]
+]
 
-    # Assert
-    expect(answer).must_equal false
-  end
+# Act
+answer = possible_bipartition(dislikes)
 
-  it "will return true for a graph which can be bipartitioned" do
-    # Arrange
-    dislikes = [ [3, 6],
-      [2, 5],
-      [1, 3],
-      [0, 2],
-      [5],
-      [1, 4],
-      [0]
-    ]
-
-    # Act
-    answer = possible_bipartition(dislikes)
-
-    # Assert
-    expect(answer).must_equal true
-  end
+# Assert
+expect(answer).must_equal true
+end
 
 
-  it "will return false for a graph which cannot be bipartitioned" do
-    # Arrange
-    dislikes = [ [3, 6],
-      [2, 5],
-      [1, 3],
-      [0, 2, 4],
-      [3, 5],
-      [1, 4],
-      [0]
-    ]
+it "will return false for a graph which cannot be bipartitioned" do
+  # Arrange
+  dislikes = [ [3, 6],
+  [2, 5],
+  [1, 3],
+  [0, 2, 4],
+  [3, 5],
+  [1, 4],
+  [0]
+]
 
-    # Act
-    answer = possible_bipartition(dislikes)
+# Act
+answer = possible_bipartition(dislikes)
 
-    # Assert
-    expect(answer).must_equal false
-  end
+# Assert
+expect(answer).must_equal false
+end
 
-  it "will work for an empty graph" do
-    expect(possible_bipartition([])).must_equal true
-  end
+it "will work for an empty graph" do
+  expect(possible_bipartition([])).must_equal true
+end
 end
